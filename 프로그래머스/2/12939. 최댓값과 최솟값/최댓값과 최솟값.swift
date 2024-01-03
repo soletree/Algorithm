b11c0ae120a579d0205255506d28482b497a617b
Hyemin Heo <97100404+soletree@users.noreply.github.com>
2024-01-04 00:37:13 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.23 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.swift
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.swift
@@ -1,8 +1,7 @@
 func solution(_ s:String) -> String {
-    var result = ""
     let arrayOfS = s.split(separator: " ").map { Int($0) ?? 0 }
     let maxOfS = arrayOfS.max()!
     let minOfS = arrayOfS.min()!
-    result = "\(minOfS) \(maxOfS)"
-    return result
+
+    return "\(minOfS) \(maxOfS)"
 }
