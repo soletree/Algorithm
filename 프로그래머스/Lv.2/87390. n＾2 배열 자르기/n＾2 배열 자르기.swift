9a14cdf4b1ee0b9dc456f6a97cc082e61b512a92
Hyemin Heo <97100404+soletree@users.noreply.github.com>
2024-01-12 02:12:37 +0900

    [level Lv.2] Title: n^2 배열 자르기, Time: 3.33 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/Lv.2/87390. n＾2 배열 자르기/n＾2 배열 자르기.swift
+++ 프로그래머스/Lv.2/87390. n＾2 배열 자르기/n＾2 배열 자르기.swift
@@ -3,12 +3,11 @@ import Foundation
 func solution(_ n: Int,
               _ left: Int64,
               _ right: Int64) -> [Int] {
-    var nByN: [[Int]] = []
     var result: [Int] = []
     
     for index in (Int(left)...Int(right)) {
         let row = index / n 
-        let col = index % n
+        let col = index % n 
         result.append(max(row+1, col+1))
     }
     
