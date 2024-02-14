1a4b16be486d935ed39ce362d0fcefeeee8af6a9
Hyemin Heo <97100404+soletree@users.noreply.github.com>
2024-02-14 10:52:28 +0900

    [level 2] Title: 가격대 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
+++ 프로그래머스/2/131530. 가격대 별 상품 개수 구하기/가격대 별 상품 개수 구하기.sql
@@ -3,5 +3,5 @@ SELECT
 floor(price / 10000) * 10000 as PRICE_GROUP,
 count(*) as PRODUCTS 
 from product
-group by floor(price / 10000) 
-order by floor(price / 10000) asc;
+group by PRICE_GROUP 
+order by PRICE_GROUP asc;
