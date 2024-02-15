7a912070c9eafad9aad33db8a7f8736700a6188e
Hyemin Heo <97100404+soletree@users.noreply.github.com>
2024-02-15 20:49:44 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 68 ms, Memory: 34072 KB -BaekjoonHub

--- 백준/Gold V/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold V/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -35,13 +35,9 @@ n, m = map(int, input().split())
 board = [0] * 101
 visited = [False] * 101
 
-for _ in range(n):
+for _ in range(n+m):
     x, y = map(int, input().split())
     board[x] = y
-
-for _ in range(m):
-    u, v = map(int, input().split())
-    board[u] = v
 
 ans = int(1e9)
 solve()
