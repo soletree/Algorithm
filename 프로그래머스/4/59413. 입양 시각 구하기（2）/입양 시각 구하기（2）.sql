06eb9965234fd580467d8fe47d225d8dc9367575
Hyemin Heo <97100404+soletree@users.noreply.github.com>
2024-02-16 17:18:16 +0900

    [level 4] Title: 입양 시각 구하기(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
@@ -0,0 +1,18 @@
+# with hour_list as (
+#     SELECT date_format(datetime, "%H") as hour, count(*)
+#     from animal_outs
+#     group by hour
+# ), 
+with recursive number as (
+    select 0 as n
+    union all
+    select n+1 from number where n < 23
+), hour_list as (
+    SELECT date_format(datetime, "%H") as hour, count(*) as cnt
+    from animal_outs
+    group by hour
+)
+
+select n.n, if(h.cnt is null, 0, h.cnt)
+from hour_list as h right join number as n
+on h.hour = n.n
